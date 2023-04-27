683cb26be1b8e78a3aac54e98e4fbb0f4a87cd95
Caio <caiusito@gmail.com>
2023-04-27 14:58:31 -0300

    feat. Req 2

--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -1,5 +1,14 @@
-# from challenges.challenge_encrypt_message import encrypt_message
+from challenges.challenge_encrypt_message import encrypt_message
+from pytest import raises
 
 
 def test_encrypt_message():
-    pass
+    assert encrypt_message('maceio', 1) == 'm_oieca'
+    assert encrypt_message('maceio', 2) == 'oiec_am'
+    assert encrypt_message('maceio', 100) == 'oiecam'
+
+    with raises(TypeError):
+        encrypt_message('maceio', 'maceio')
+
+    with raises(TypeError):
+        encrypt_message(-1, -1)
