5e00a53493f76e4871c9af723a1625f763e40133
Caio <caiusito@gmail.com>
2023-04-27 15:45:28 -0300

    Feat. Req 3.3 / valida se nenhuma palavra for passada por parâmetro

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,5 +1,7 @@
 def is_palindrome_recursive(word, low_index, high_index):
     if len(word) == 0:
         return False
+    elif word[low_index] != word[high_index]:
+        return False
     else:
-        return True
+        return is_palindrome_recursive(word, low_index, high_index)
