ca5e0d5800f0f183df46117939b946ff548316e4
Caio <caiusito@gmail.com>
2023-04-27 14:32:13 -0300

    feat. Req 1

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,13 @@
 def study_schedule(permanence_period, target_time):
-    """Bora começar"""
-    raise NotImplementedError
+    if target_time is None:
+        return None
+
+    counter = 0
+
+    for item in permanence_period:
+        if type(item[0]) != int or type(item[1]) != int:
+            return None
+        if item[0] <= target_time <= item[1]:
+            counter += 1
+
+    return counter
